04b5acd384d78e6fbcd74a1f6cfdd5a1ca674c14
Patrick Feniza <feniza.232177@fairview.sti.edu.ph>
2024-07-30 22:03:54 +0800

    DesigningLandingPage

--- PharmacySystem/LandingPage.Designer.vb
+++ PharmacySystem/LandingPage.Designer.vb
@@ -24,33 +24,27 @@ Partial Class LandingPage
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LandingPage))
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.GunaLabel1 = New Guna.UI.WinForms.GunaLabel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnSales = New Guna.UI.WinForms.GunaAdvenceButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(110, Byte), Integer), CType(CType(172, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(89, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(195, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.btnSales)
         Me.Panel1.Controls.Add(Me.GunaLabel1)
         Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Panel3)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(361, 1080)
         Me.Panel1.TabIndex = 0
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
-        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.PictureBox1.Location = New System.Drawing.Point(97, 24)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(168, 166)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
         '
         'GunaLabel1
         '
@@ -63,6 +57,65 @@ Partial Class LandingPage
         Me.GunaLabel1.Text = "Patche POS System"
         Me.GunaLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
+        'Panel2
+        '
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(361, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1559, 1080)
+        Me.Panel2.TabIndex = 1
+        '
+        'btnSales
+        '
+        Me.btnSales.AnimationHoverSpeed = 0.07!
+        Me.btnSales.AnimationSpeed = 0.03!
+        Me.btnSales.BaseColor = System.Drawing.Color.FromArgb(CType(CType(89, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(195, Byte), Integer))
+        Me.btnSales.BorderColor = System.Drawing.Color.Transparent
+        Me.btnSales.BorderSize = 1
+        Me.btnSales.ButtonType = Guna.UI.WinForms.AdvenceButtonType.RadioButton
+        Me.btnSales.CheckedBaseColor = System.Drawing.Color.FromArgb(CType(CType(134, Byte), Integer), CType(CType(167, Byte), Integer), CType(CType(137, Byte), Integer))
+        Me.btnSales.CheckedBorderColor = System.Drawing.Color.Transparent
+        Me.btnSales.CheckedForeColor = System.Drawing.Color.Black
+        Me.btnSales.CheckedImage = Global.PharmacySystem.My.Resources.Resources.increase
+        Me.btnSales.CheckedLineColor = System.Drawing.Color.FromArgb(CType(CType(134, Byte), Integer), CType(CType(167, Byte), Integer), CType(CType(137, Byte), Integer))
+        Me.btnSales.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.btnSales.FocusedColor = System.Drawing.Color.Empty
+        Me.btnSales.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSales.ForeColor = System.Drawing.Color.White
+        Me.btnSales.Image = Global.PharmacySystem.My.Resources.Resources.increase
+        Me.btnSales.ImageSize = New System.Drawing.Size(20, 20)
+        Me.btnSales.LineColor = System.Drawing.Color.FromArgb(CType(CType(110, Byte), Integer), CType(CType(172, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.btnSales.Location = New System.Drawing.Point(12, 289)
+        Me.btnSales.Name = "btnSales"
+        Me.btnSales.OnHoverBaseColor = System.Drawing.Color.FromArgb(CType(CType(155, Byte), Integer), CType(CType(207, Byte), Integer), CType(CType(83, Byte), Integer))
+        Me.btnSales.OnHoverBorderColor = System.Drawing.Color.Transparent
+        Me.btnSales.OnHoverForeColor = System.Drawing.Color.Black
+        Me.btnSales.OnHoverImage = Global.PharmacySystem.My.Resources.Resources.increaseWhite
+        Me.btnSales.OnHoverLineColor = System.Drawing.Color.FromArgb(CType(CType(155, Byte), Integer), CType(CType(207, Byte), Integer), CType(CType(83, Byte), Integer))
+        Me.btnSales.OnPressedColor = System.Drawing.Color.FromArgb(CType(CType(155, Byte), Integer), CType(CType(207, Byte), Integer), CType(CType(83, Byte), Integer))
+        Me.btnSales.Size = New System.Drawing.Size(334, 50)
+        Me.btnSales.TabIndex = 2
+        Me.btnSales.Text = "Sales"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(97, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(168, 166)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Location = New System.Drawing.Point(4, 3)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(353, 266)
+        Me.Panel3.TabIndex = 3
+        '
         'LandingPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
@@ -70,6 +123,7 @@ Partial Class LandingPage
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1920, 1080)
         Me.ControlBox = False
+        Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -86,4 +140,7 @@ Partial Class LandingPage
     Friend WithEvents Panel1 As Panel
     Friend WithEvents GunaLabel1 As Guna.UI.WinForms.GunaLabel
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnSales As Guna.UI.WinForms.GunaAdvenceButton
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
 End Class
